6342e775a71d28cb2b67ccd7f9fa46477890e3aa
anamritraj <anandamritraj904@gmail.com>
2019-05-16 01:35:17 +0530

    Fix the random team selection logic.

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ class Round {
   constructor(teams) {
     this.teams = teams;
     this.noOfTeams = teams.length;
-    this.noOfMatches = this.noOfTeams / 2;
+    this.noOfMatches = Math.floor(this.noOfTeams / 2);
     this.matches = [];
     this.teamsAlreadyScheduled = new Set();
     this.winners = [];
@@ -58,27 +58,37 @@ class Round {
 
     for (let index = 0; index < this.noOfMatches; index++) {
       // Create a match between two teams.
-      let match = new Match(this.getRandomTeamId(), this.getRandomTeamId());
+      let match = new Match(
+        this.teams[2 * index].teamId,
+        this.teams[2 * index + 1].teamId
+      );
       this.matches.push(match);
     }
+
+    this.showRoundDetails();
+    console.log("The matches are", this.matches);
   };
+
+  showRoundDetails() {
+    // This function will update the dom to show match details
+  }
 
   markTeamAsScheduled(teamIndex) {
     this.teamsAlreadyScheduled.add(teamIndex);
   }
 
-  getRandomTeamId() {
-    let randomTeamId = this.teams[Math.floor(Math.random() * this.noOfTeams)]
-      .teamId;
+  // getRandomTeamId(id = 0) {
+  //   let randomTeamId = this.teams[id].teamId;
 
-    while (this.teamsAlreadyScheduled.has(randomTeamId)) {
-      randomTeamId = this.teams[Math.floor(Math.random() * this.noOfTeams)]
-        .teamId;
-    }
-    this.markTeamAsScheduled(randomTeamId);
+  //   while (this.teamsAlreadyScheduled.has(randomTeamId)) {
+  //     console.log(id);
+  //     randomTeamId = this.teams[id].teamId;
+  //     id++;
+  //   }
+  //   this.markTeamAsScheduled(randomTeamId);
 
-    return randomTeamId;
-  }
+  //   return randomTeamId;
+  // }
 
   startRoundMatches() {
     this.scheduleMatches();
@@ -129,6 +139,7 @@ class Tournament {
 
     round.startRoundMatches().then(winners => {
       self.teams = winners;
+      console.log("Round Winners", winners);
       if (winners.length > 1) {
         setTimeout(() => self.startTournament(), 0);
       } else {
@@ -140,20 +151,228 @@ class Tournament {
 
 let teamList = [
   {
-    teamId: 1,
-    teamName: "Team 1"
+    teamId: 1610612737,
+    abbreviation: "ATL",
+    teamName: "Atlanta Hawks",
+    simpleName: "Hawks",
+    location: "Atlanta"
   },
   {
-    teamId: 2,
-    teamName: "Team 2"
+    teamId: 1610612738,
+    abbreviation: "BOS",
+    teamName: "Boston Celtics",
+    simpleName: "Celtics",
+    location: "Boston"
   },
   {
-    teamId: 3,
-    teamName: "Team 3"
+    teamId: 1610612751,
+    abbreviation: "BKN",
+    teamName: "Brooklyn Nets",
+    simpleName: "Nets",
+    location: "Brooklyn"
   },
   {
-    teamId: 4,
-    teamName: "Team 4"
+    teamId: 1610612766,
+    abbreviation: "CHA",
+    teamName: "Charlotte Hornets",
+    simpleName: "Hornets",
+    location: "Charlotte"
+  },
+  {
+    teamId: 1610612741,
+    abbreviation: "CHI",
+    teamName: "Chicago Bulls",
+    simpleName: "Bulls",
+    location: "Chicago"
+  },
+  {
+    teamId: 1610612739,
+    abbreviation: "CLE",
+    teamName: "Cleveland Cavaliers",
+    simpleName: "Cavaliers",
+    location: "Cleveland"
+  },
+  {
+    teamId: 1610612742,
+    abbreviation: "DAL",
+    teamName: "Dallas Mavericks",
+    simpleName: "Mavericks",
+    location: "Dallas"
+  },
+  {
+    teamId: 1610612743,
+    abbreviation: "DEN",
+    teamName: "Denver Nuggets",
+    simpleName: "Nuggets",
+    location: "Denver"
+  },
+  {
+    teamId: 1610612765,
+    abbreviation: "DET",
+    teamName: "Detroit Pistons",
+    simpleName: "Pistons",
+    location: "Detroit"
+  },
+  {
+    teamId: 1610612744,
+    abbreviation: "GSW",
+    teamName: "Golden State Warriors",
+    simpleName: "Warriors",
+    location: "Golden State"
+  },
+  {
+    teamId: 1610612745,
+    abbreviation: "HOU",
+    teamName: "Houston Rockets",
+    simpleName: "Rockets",
+    location: "Houston"
+  },
+  {
+    teamId: 1610612754,
+    abbreviation: "IND",
+    teamName: "Indiana Pacers",
+    simpleName: "Pacers",
+    location: "Indiana"
+  },
+  {
+    teamId: 1610612746,
+    abbreviation: "LAC",
+    teamName: "Los Angeles Clippers",
+    simpleName: "Clippers",
+    location: "Los Angeles"
+  },
+  {
+    teamId: 1610612747,
+    abbreviation: "LAL",
+    teamName: "Los Angeles Lakers",
+    simpleName: "Lakers",
+    location: "Los Angeles"
+  },
+  {
+    teamId: 1610612763,
+    abbreviation: "MEM",
+    teamName: "Memphis Grizzlies",
+    simpleName: "Grizzlies",
+    location: "Memphis"
+  },
+  {
+    teamId: 1610612748,
+    abbreviation: "MIA",
+    teamName: "Miami Heat",
+    simpleName: "Heat",
+    location: "Miami"
+  },
+  {
+    teamId: 1610612749,
+    abbreviation: "MIL",
+    teamName: "Milwaukee Bucks",
+    simpleName: "Bucks",
+    location: "Milwaukee"
+  },
+  {
+    teamId: 1610612750,
+    abbreviation: "MIN",
+    teamName: "Minnesota Timberwolves",
+    simpleName: "Timberwolves",
+    location: "Minnesota"
+  },
+  {
+    teamId: 1610612740,
+    abbreviation: "NOP",
+    teamName: "New Orleans Pelicans",
+    simpleName: "Pelicans",
+    location: "New Orleans"
+  },
+  {
+    teamId: 1610612752,
+    abbreviation: "NYK",
+    teamName: "New York Knicks",
+    simpleName: "Knicks",
+    location: "New York"
+  },
+  {
+    teamId: 1610612760,
+    abbreviation: "OKC",
+    teamName: "Oklahoma City Thunder",
+    simpleName: "Thunder",
+    location: "Oklahoma City"
+  },
+  {
+    teamId: 1610612753,
+    abbreviation: "ORL",
+    teamName: "Orlando Magic",
+    simpleName: "Magic",
+    location: "Orlando"
+  },
+  {
+    teamId: 1610612755,
+    abbreviation: "PHI",
+    teamName: "Philadelphia 76ers",
+    simpleName: "76ers",
+    location: "Philadelphia"
+  },
+  {
+    teamId: 1610612756,
+    abbreviation: "PHX",
+    teamName: "Phoenix Suns",
+    simpleName: "Suns",
+    location: "Phoenix"
+  },
+  {
+    teamId: 1610612757,
+    abbreviation: "POR",
+    teamName: "Portland Trail Blazers",
+    simpleName: "Trail Blazers",
+    location: "Portland"
+  },
+  {
+    teamId: 1610612758,
+    abbreviation: "SAC",
+    teamName: "Sacramento Kings",
+    simpleName: "Kings",
+    location: "Sacramento"
+  },
+  {
+    teamId: 1610612759,
+    abbreviation: "SAS",
+    teamName: "San Antonio Spurs",
+    simpleName: "Spurs",
+    location: "San Antonio"
+  },
+  {
+    teamId: 1610612761,
+    abbreviation: "TOR",
+    teamName: "Toronto Raptors",
+    simpleName: "Raptors",
+    location: "Toronto"
+  },
+  {
+    teamId: 1610612762,
+    abbreviation: "UTA",
+    teamName: "Utah Jazz",
+    simpleName: "Jazz",
+    location: "Utah"
+  },
+  {
+    teamId: 1610612764,
+    abbreviation: "WAS",
+    teamName: "Washington Wizards",
+    simpleName: "Wizards",
+    location: "Washington"
+  },
+  {
+    teamId: 1610612723,
+    abbreviation: "DAS",
+    teamName: "Dallas Wizards",
+    simpleName: "Dallas",
+    location: "Denver"
+  },
+  {
+    teamId: 1615612764,
+    abbreviation: "TTA",
+    teamName: "Toronto Tyrants",
+    simpleName: "Tyrants",
+    location: "Toronto"
   }
 ];
 
